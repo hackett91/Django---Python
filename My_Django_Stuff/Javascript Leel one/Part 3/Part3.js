0ea4bbec7f7352458d1cfb0b644ca57acd45756f
hackett91 <C16723031@mydit.ie>
2018-08-08 00:31:24 +0100

    javascript basics

---
+++ My_Django_Stuff/Javascript Leel one/Part 3/Part3.js
@@ -0,0 +1,4 @@
+var inputPounds = prompt("Please insert the weight in pounds (lbs): ");
+var outputKilograms = inputPounds * 0.454;
+alert(inputPounds+"lbs = "+ outputKilograms+" kilograms");
+console.log("Conversion Completed");
